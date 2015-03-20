801b33ce9563eb98dfaaf03e4887cc9c806cdd16
spemmons <s.p.emmons@gmail.com>
2015-03-20 15:46:03 -0500

    adjust logging

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ var config = require('./config');
 var m2m = require('m2m-ota-javascript');
 var Listener = require('./lib/listener');
 
-var heartbeat = new m2m.Message({messageType: m2m.Common.MOBILE_ORIGINATED_EVENT}).pushString(0,config.imei);
+var CLOSING_DELAY = 2;
+
+var heartbeat = new m2m.Message({messageType: m2m.Common.MOBILE_ORIGINATED_EVENT,sequenceNumber: 1}).pushString(0,config.imei);
 
 var listener = new Listener(config.port,config.host,function(message){
+    console.log(message.timestamp.toString() + ' - received ACK...     ' + message.sequenceNumber);
     if ((heartbeat.sequenceNumber += 1) < 5)
         sendHeartbeat();
-    else
-        listener.client.close();
+    else {
+        console.log((new Date()).valueOf().toString() + ' - closing in ' + CLOSING_DELAY + '...');
+        setTimeout(function() { listener.client.close(); },CLOSING_DELAY * 1000);
+    }
 });
 
 function sendHeartbeat(){
     heartbeat.timestamp = (new Date()).valueOf();
-    console.log('sending heartbeat...' + heartbeat.sequenceNumber + ': ' + heartbeat.toWire().inspect());
+    console.log(heartbeat.timestamp.toString() + ' - sending heartbeat...' + heartbeat.sequenceNumber);
     listener.send(heartbeat);
 }
 
